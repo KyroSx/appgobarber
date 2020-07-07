acfe26652a898319e5d766800bec305a8cda5d04
leonardo <leolub150@gmail.com>
2020-07-07 13:47:42 -0300

    create aux components base

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-
 import { Image } from 'react-native';
-import { Container } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logo from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <>
       <Container>
         <Image source={logo} />
+        <Title>Faça seu logon</Title>
+
+        <Input />
+        <Input />
+
+        <Button />
       </Container>
     </>
   );
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotSlab-medium';
+  padding: 64px 0 24px 0;
+`;
